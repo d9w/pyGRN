36c6834de795333b956d897771d30e3dae85d757
Dennis Wilson <dennis@d9w.xyz>
2018-03-09 12:40:53 +0100

    test for import and export; fixes #4

---
+++ tests/test_export.py
@@ -0,0 +1,27 @@
+from pygrn import grns
+import json
+import numpy as np
+
+
+def test_export():
+    grn = grns.ClassicGRN()
+    grn.random(10, 10, 10)
+    str_grn = str(grn)
+    assert str_grn == json.dumps({'ids': grn.identifiers.tolist(),
+                                  'enh': grn.enhancers.tolist(),
+                                   'inh': grn.inhibitors.tolist(),
+                                   'beta': grn.beta,
+                                   'delta': grn.delta})
+
+
+def test_import():
+    grn = grns.ClassicGRN()
+    grn.random(10, 10, 10)
+    str_grn = str(grn)
+    grn2 = grns.ClassicGRN()
+    grn2.from_str(str_grn)
+    assert np.all(grn.identifiers == grn2.identifiers)
+    assert np.all(grn.enhancers == grn2.enhancers)
+    assert np.all(grn.inhibitors == grn2.inhibitors)
+    assert grn.beta == grn2.beta
+    assert grn.delta == grn2.delta
