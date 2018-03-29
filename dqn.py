584b13afdeda36dbcdb9693a91388228529a786d
Dennis Wilson <dennis@d9w.xyz>
2018-03-29 11:29:31 +0200

    grn file backup for dqn

--- dqn.py
+++ dqn.py
@@ -16,6 +16,7 @@ parser.add_argument('--env', type=str, help='Gym environment', default='CartPole
 parser.add_argument('--steps', type=int, help='Number of training steps', default=10000)
 parser.add_argument('--warmup', type=int, help='Number of warmup steps', default=200)
 parser.add_argument('--gens', type=int, help='Number of generations', default=50)
+parser.add_argument('--grn_file', type=str, help='Experts from GRN file', default='')
 parser.add_argument('--grn_dir', type=str, help='Directory for storing GRNS',
                     default='grns')
 parser.add_argument('--log_dir', type=str, help='Log directory', default='logs')
@@ -29,10 +30,30 @@ newgrn = lambda: grns.DiffGRN()
 if args.evo:
     grneat = evolution.Evolution(p, newgrn, args.id, grn_dir=args.grn_dir,
                                  log_dir=args.log_dir)
+    if args.grn_file:
+        with open(args.grn_file, 'r') as f:
+            pcount = 0
+            for g in f.readlines:
+                if pcount < len(grneat.population.offspring):
+                    grn = newgrn()
+                    grn.from_str(g)
+                    ind = evolution.Individual(grn)
+                    grneat.population[pcount] = ind
+                    pcount += 1
     grneat.run(args.gens)
 else:
-    for i in range(20):
-        grn = newgrn()
-        grn.random(p.nin, p.nout, 1)
-        p.generation = i
-        p.eval(grn)
+    if args.grn_file:
+        with open(args.grn_file, 'r') as f:
+            gen = 0
+            for g in f.readlines:
+                grn = newgrn()
+                grn.from_str(g)
+                p.generation = gen
+                p.eval(grn)
+                gen += 1
+    else:
+        for i in range(20):
+            grn = newgrn()
+            grn.random(p.nin, p.nout, 1)
+            p.generation = i
+            p.eval(grn)
