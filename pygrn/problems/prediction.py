663134859952c14d7ba6d0e7262a041e4e13aaf9
Dennis Wilson <dennis@d9w.xyz>
2018-05-17 12:00:10 +0200

    more options, live scaling and error report

--- pygrn/problems/prediction.py
+++ pygrn/problems/prediction.py
@@ -4,6 +4,8 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM, SimpleRNN
 from keras.optimizers import Adam
 from keras import backend as K
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 import numpy as np
 import os
 import pandas as pd
@@ -15,28 +17,39 @@ class Prediction(Problem):
 
     def __init__(self, log_file, seed=0, learn=True, batch_size=1,
                  epochs=1, data_dir='./', lamarckian=False, unsupervised=True,
-                 stateful=True, model='RGRN', ntrain=75000):
-        train_data_file = os.path.join(data_dir, 'kliens_train.csv')
-        test_data_file = os.path.join(data_dir, 'kliens_train.csv')
-        train = pd.read_csv(train_data_file).values
-        test = pd.read_csv(test_data_file).values
+                 stateful=True, model='RGRN', ntrain=6*24*60, ntest=24*60,
+                 shift=1, lag=60):
+        rawvals = pd.read_csv(os.path.join(data_dir, 'kliens_raw.csv'))
+        self.df = rawvals
+        if shift != 0:
+            self.df = rawvals - rawvals.shift(-1)
+        self.df['target'] = rawvals['close'] - rawvals['close'].shift(-shift-lag)
+        self.df.dropna(inplace=True)
 
-        ntrain = len(train)-ntrain
-        ntest = round(ntrain/3.0)
-        X, y = train[:-ntrain, 0:-1], train[:-ntrain, -1]
-        X = X.reshape(X.shape[0], 1, X.shape[1])
-        Xtest, ytest = test[:ntest, 0:-1], test[:ntest, -1]
-        Xtest = Xtest.reshape(Xtest.shape[0], 1, Xtest.shape[1])
+        train = self.df.tail(ntrain+ntest).head(ntrain)
+        test = self.df.tail(ntest)
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.scaler = self.scaler.fit(train)
+        train = self.scaler.transform(train)
+        test = self.scaler.transform(test)
 
-        self.x_train = X
-        self.y_train = y
-        self.x_test = Xtest
-        self.y_test = ytest
+        self.x_train = train[:, 0:-1]
+        self.x_train = self.x_train.reshape(self.x_train.shape[0], 1, self.x_train.shape[1])
+        self.y_train = train[:, -1]
+
+        self.x_test = test[:, 0:-1]
+        self.x_test = self.x_test.reshape(self.x_test.shape[0], 1, self.x_test.shape[1])
+        self.y_test = test[:, -1]
 
         self.batch_size = batch_size
         self.epochs = epochs
         self.learn = learn
         self.generation = 0
+        self.ntrain = ntrain
+        self.ntest = ntest
+        self.shift = shift
+        self.lag = lag
+        self.seed = seed
         self.lamarckian = lamarckian
         self.unsupervised = unsupervised
         self.stateful = stateful
@@ -45,8 +58,8 @@ class Prediction(Problem):
             i = np.random.randint(self.x_train.shape[2])
             self.y_train = 0.5*self.x_train[:,0,i] + 0.5*np.random.rand(len(self.y_train))
 
-        self.nin = X.shape[2]
-        self.nout = 20
+        self.nin = self.x_train.shape[2]
+        self.nout = 10
         self.cacheable = False
         self.logfile = log_file
 
@@ -57,13 +70,19 @@ class Prediction(Problem):
             self.y_train = 0.5*self.x_train[:,0,i] + 0.5*np.random.rand(len(self.y_train))
 
     def eval(self, grn):
+        seed = np.random.randint(1e5)
         model = Sequential()
         batch_input_shape=(self.batch_size, self.x_train.shape[1], self.x_train.shape[2])
+        start_time = datetime.now()
         if self.model_type == LSTM or self.model_type == SimpleRNN:
-            layer = self.model_type(self.nout, stateful=True, batch_input_shape=batch_input_shape)
+            np.random.seed(round(self.grn.identifiers[0]*100))
+            layer = self.model_type(self.nout, stateful=self.stateful,
+                                    batch_input_shape=batch_input_shape)
             model.add(layer)
         else:
-            layer = self.model_type(str(grn), stateful=True, batch_input_shape=batch_input_shape)
+            np.random.seed(self.seed)
+            layer = self.model_type(str(grn), stateful=self.stateful,
+                                    batch_input_shape=batch_input_shape)
             model.add(layer)
         model.add(Dense(1))
         model.compile(loss='mean_squared_error', optimizer='adam')
@@ -79,24 +98,41 @@ class Prediction(Problem):
                     for l in range(len(history.history['loss'])):
                         train_fit = history.history['loss'][l]
                         error += [train_fit]
-                        f.write('L,%s,%d,%d,%e\n' % (datetime.now().isoformat(),
-                                                    self.generation, i,
-                                                    train_fit))
+                        f.write('L,%e,%d,%s,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%e\n' % (
+                            (datetime.now()-start_time).total_seconds(),
+                            self.seed, self.model_type.__name__, self.epochs,
+                            self.lamarckian, self.unsupervised, self.stateful,
+                            self.ntrain, self.ntest, self.shift, self.lag,
+                            self.generation, i, train_fit))
             # lamarckian evolution
             if self.lamarckian:
                 layer.set_learned_genes(grn)
         start_error = error[0]
         end_error = error[-1]
         fit = start_error - end_error
+        total_error = np.sum(np.abs(error))
         if not self.unsupervised:
-            fit = -model.evaluate(self.x_test, self.y_test,
-                                  batch_size=self.batch_size, verbose=0)
-
+            # predict and return unscaled difference
+            ntest = len(self.y_test)
+            yhat = model.predict(self.x_test, batch_size=self.batch_size, verbose=0)
+            final = np.concatenate((
+                np.reshape(self.x_test, (self.x_test.shape[0], self.x_test.shape[2])),
+                np.reshape(yhat, (ntest,1))), axis=1)
+            inverted = self.scaler.inverse_transform(final)
+            yhatp = inverted[:, -1]
+            target = self.df['target'].tail(ntest)
+            fit = np.sqrt(mean_squared_error(yhatp, target))
+            total_error = np.sum(np.abs(yhatp - target))
         if np.isnan(fit):
             fit = -1e10
         with open(self.logfile, 'a') as f:
-            f.write('M,%s,%d,%e,%e,%e\n' % (datetime.now().isoformat(),
-                                            self.generation, start_error, end_error, fit))
+            f.write('M,%e,%d,%s,%d,%d,%d,%d,%d,%d,%d,%d,%d,%e,%e,%e,%e\n' % (
+                (datetime.now()-start_time).total_seconds(),
+                self.seed, self.model_type.__name__, self.epochs,
+                self.lamarckian, self.unsupervised, self.stateful,
+                self.ntrain, self.ntest, self.shift, self.lag,
+                self.generation, start_error, end_error, total_error, fit))
         del model
         K.clear_session()
+        np.random.seed(seed)
         return fit
--- regression.py
+++ regression.py
@@ -1,4 +1,4 @@
-from pygrn import grns, problems, evolution
+from pygrn import grns, problems, evolution, config
 import argparse
 import os
 
@@ -17,8 +17,12 @@ parser.add_argument('--stateful', dest='stateful', action='store_const',
                     const=True, default=False, help='Stateful model')
 parser.add_argument('--id', type=str, help='Run id for logging')
 parser.add_argument('--model', type=str, help='Model')
-parser.add_argument('--train_size', type=int, default=75000,
-                    help='Number of training samples')
+parser.add_argument('--ntrain', type=int, default=6*24*60, help='Number of training samples')
+parser.add_argument('--ntest', type=int, default=24*60, help='Number of testing samples')
+parser.add_argument('--shift', type=int, default=1, help='Data shift')
+parser.add_argument('--lag', type=int, default=60, help='Time step for prediction')
+parser.add_argument('--nreg', type=int, default=10,
+                    help='Number of starting regulatory proteins')
 parser.add_argument('--seed', type=int, help='Random seed',
                     default=0)
 parser.add_argument('--problem', type=str, help='Problem',
@@ -38,11 +42,14 @@ grn_dir = os.path.join(args.root_dir, 'grns')
 data_dir = os.path.join(args.root_dir, 'data')
 log_file = os.path.join(log_dir, 'fits_' + args.id + '.log')
 
+config.START_REGULATORY_SIZE = args.nreg
+
 p = eval('problems.' + args.problem)
 p = p(log_file, seed=args.seed, learn=args.learn, epochs=args.epochs,
       data_dir=data_dir, lamarckian=args.lamarckian,
       unsupervised=args.unsupervised, stateful=args.stateful,
-      model=args.model, ntrain=args.train_size)
+      model=args.model, ntrain=args.ntrain, ntest=args.ntest,
+      shift=args.shift, lag=args.lag)
 
 newgrn = lambda: grns.DiffGRN()
 if args.evo:
@@ -54,11 +61,11 @@ elif args.grn_file:
         for g in f.readlines():
             grn = newgrn()
             grn.from_str(g)
-            p.generation = args.generation
+            p.generation_function(None, args.generation)
             p.eval(grn)
 else:
     for i in range(20):
         grn = newgrn()
-        grn.random(p.nin, p.nout, 20)
-        p.generation = i
+        grn.random(p.nin, p.nout, args.nreg)
+        p.generation_function(None, i)
         p.eval(grn)
