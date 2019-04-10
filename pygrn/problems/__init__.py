5fd58e3f73d658d886a042c97faac239f74f37f6
Dennis Wilson <dennis@d9w.xyz>
2019-04-10 20:00:11 +0200

    removal of other problem types for dependencies, need to make external

--- pygrn/problems/__init__.py
+++ pygrn/problems/__init__.py
@@ -1,11 +1,11 @@
 from .base import Random, Static, Counter, TFRandom
 from .regression import Regression
-from .time_regression import TimeRegression
-from .double import DoubleFrequency
-from .boston import Boston
-from .eeg import EEG
-from .energy import Energy
-from .air_quality import AirQuality
-from .dqn import SLGym, Gym, Atari
-from .prediction import Prediction
-from .text import TextGen
+# from .time_regression import TimeRegression
+# from .double import DoubleFrequency
+# from .boston import Boston
+# from .eeg import EEG
+# from .energy import Energy
+# from .air_quality import AirQuality
+# from .dqn import SLGym, Gym, Atari
+# from .prediction import Prediction
+# from .text import TextGen
